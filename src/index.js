b71e87809b07e9348bbe53c594440be1ff91afe7
MeoDaoSi <102019281+MeoDaoSi@users.noreply.github.com>
2023-10-02 16:00:38 +0700

    ADD handlebar

--- src/index.js
+++ src/index.js
@@ -4,30 +4,38 @@ const cors = require('cors');
 const router = require('./routes/index')
 const AppError = require('./utils/AppError');
 const errorHandler = require('./utils/errorHandler');
+const { engine } = require('express-handlebars');
 
 const app = express();
 
-// config public directory
+// Config public directory
 app.use(express.static(`${__dirname}/../public`))
-// parses the application/json content type
+// Parses the application/json content type
 app.use(express.json())
 // [Middleware] - enable cross-origin
 app.use(cors());
-// config environment variable
+// Config environment variable
 require('dotenv').config();
-// connect DB
+// Connect DB
 const db = require('./db/index');
 db.connect();
+// [Template] - Handlebar
+app.engine('.hbs', engine({
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+app.set('views', `${__dirname}/resources/views`);
 
+// Route welcome!
 app.get('/', (req, res) => {
-    res.json('Welcome!')
+    res.render('home')
 })
 
 // Router
 app.use(router);
 // [Handle if not found api]
 app.use((req, res, next) => {
-    return next(new AppError(404,"Resource not found"))
+    return next(new AppError(404,'Resource not found'))
 })
 // [Call errorHandler]
 app.use(errorHandler);
